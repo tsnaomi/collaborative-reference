76f1284a4c4b3d00facd76fa951f3a2a591969f3
Naomi T. S <tsnaomi@uw.edu>
2015-05-24 20:33:59 -0700

    implement generation of reference instances

--- baseline_games.py
+++ baseline_games.py
@@ -1,35 +1,13 @@
 from ibr_classifer import ibr_classifier
 
 from collections import namedtuple
-from itertools import combinations_with_replacement as cwr, permutations
-from pprint import pprint
-
-# SUNDAY at 5 :D
-
-# NOTES:
-# - Do your implementations in NEW files (we'll compile them later)
-# - Pass around strings
-# - Use the class' Neural Network (we'll modify it later)
-
-# TASKS:
-# Task 1: Generate game instances and classify them -- Naomi
-# Task 2: Implement Speaker module (produce() and learn()) -- Phil
-# Task 3: Implement Listerner module (interpret() and learn())  -- Ciyang
-
-# PROJECT MILESTONE:
-# (due May 25, 11:59 pm)
-# A short overview of your project including at least the following
-# information:
-#   - A statement of the project's goals.
-#   - A summary of previous approaches (drawing on the lit review).
-#   - A summary of the current approach.
-#   - A summary of progress so far: what you have been done, what you still
-#     need to do, and any obstacles or concerns that might prevent your project
-#     from coming to fruition.
+from itertools import (
+    combinations_with_replacement as combin,
+    permutations,
+    repeat,
+    )
 
 Game = namedtuple('Game', ['messages', 'targets', 'sems'])  # G = (M, T, [.])
-
-RefInstance = namedtuple('RefInstance', ['game', 'message', 'target'])
 
 Features = ['hats', 'glasses', 'mustache']
 
@@ -54,45 +32,92 @@ Sems = {
     }
 
 
-def all_games():
-    '''Produce a list of every possible Game instance.'''
+def generate_classified_reference_instances(F=3, T=3):
+    '''Generate and classify every possible reference instance.
+
+    This generates reference instances give F number of feautres and T number
+    of targets. This function returns three lists corresponding to Level 0,
+    Level 1, and Level 2 instances.
+    '''
+    games = _all_games(F, T)
+    reference_instances = _all_reference_instances(games, F, T)
+
+    return _classify_reference_instances(reference_instances)
+
+
+def _all_games(F, T):
+    # this function produces a list of every possible Game instance, given F
+    # number of features and T number of targets
 
     def get_targets(vector):
+        if F != 3 or T != 3:
+            return {'t%s' % i: vector[i * F:(i * F) + F] for i in range(t)}
+
         return {'left': vector[:3], 'center': vector[3:6], 'right': vector[6:]}
 
     def get_sems(targets):
-        sems = {0: [], 1: [], 2: [], }  # hats, glasses, mustache
+        sems = {i: [] for i in range(F)}
 
         for k, v in targets.iteritems():
             for i, f in enumerate(v):
                 if f:
                     sems[i].append(k)
 
+        if f != 3 or t != 3:
+            return {'m%s' % i: sems[i] for i in range(f)}
+
         return {'hats': sems[0], 'glasses': sems[1], 'mustache': sems[2]}
 
-    vectors = set([p for c in cwr(range(2), 9) for p in permutations(c)])
-    vectors = [list(v) for v in vectors]
+    vectors = [p for c in combin(range(2), F * T) for p in permutations(c)]
+    vectors = [list(v) for v in set(vectors)]
     targets = [get_targets(vector) for vector in vectors]
+
     return [Game(Messages, t, sems=get_sems(t)) for t in targets]
 
 
-def all_reference_instances(games=all_games()):
-    '''Produce a list of every possible reference instance given a set of games.
+def _all_reference_instances(games, F, T):
+    # this function produces a list of every possible reference instance given
+    # a list of games, F number of features, and T number of targets
 
-    A reference instance is a Game G, a message m, and intended target t.
-    '''
+    # each reference instance is a game 'game', a message 'message', and an
+    # an intended target 'target', returned in the form of a dictionary
 
     def get_ref(game, message, target):
         return {'game': game, 'message': message, 'target': target}
 
-    messages = Messages.keys()
-    targets = Targets.keys()
+    messages = _create_messages_dict(F=F).keys()
+    targets = Targets.keys() if T == 3 else ['t%s' % i for i in range(T)]
 
     return [get_ref(g, m, t) for g in games for m in messages for t in targets]
 
 
-def classify_reference_instances(ref_instances=all_reference_instances()):
-    '''Return three lists for level 0, 1, and 2 reference instances.'''
+def _create_messages_dict(F):
+    # this function creates a messages dictionary given F number of identifying
+    # features
+
+    # e,g,, if F=4, this will return the following key-value pairs:
+    #       'm0': [1, 0, 0, 0],
+    #       'm1': [0, 1, 0, 0],
+    #       'm2': [0, 0, 1, 0],
+    #       'm3': [0, 0, 0, 1],
+
+    if F == 3:
+        return Messages
+
+    messages = {'m%s' % i: list(repeat(0, F)) for i in range(F)}
+
+    for i in range(F):
+        Messages['m%s' % i][i] = 1
+
+    return messages
+
+
+def _classify_reference_instances(ref_instances):
+    '''Return three lists of level 0, 1, and 2 reference instances.'''
+    # given a list of reference instances, this function returns three lists
+    # corresponding to Level 0, Level 1, and Level 2 instances, and discards
+    # level -1 instances (see ibr_classifier)
+
     level = {-1: [], 0: [], 1: [], 2: []}
 
     for i in ref_instances:
@@ -100,7 +125,7 @@ def classify_reference_instances(ref_instances=all_reference_instances()):
 
     return level[0], level[1], level[2]
 
-level_0, level_1, level_2 = classify_reference_instances()
+level_0, level_1, level_2 = generate_classified_reference_instances()
 
 
 class Listener(object):
