7cc85f67fd417720ad86ed201b19090675a690ae
Ciyang <ciyang@server.fake>
2015-05-25 19:00:23 -0700

    position-based speaker

--- agents.py
+++ agents.py
@@ -2,6 +2,7 @@ import numpy as np
 from distributedwordreps import ShallowNeuralNetwork
 import random
 import sys
+from games import level0, level1, level2
 
 class ANNListener(object):
 
@@ -47,7 +48,7 @@ class ANNListener(object):
                 message = speaker.produce(game, target)
                 input_message = game.messages[message]
                 input_ann = input_targets + input_message
-                output_ann = np.zeros(len(game.messages))
+                output_ann = np.zeros(len(game.targets))
                 output_ann[indx] = 1.0
                 data.append((input_ann, output_ann))
         if verbose:
@@ -57,15 +58,21 @@ class ANNListener(object):
 
 class ANNSpeaker(object):
     # there could be two versions, one that represents the intended target
-    # using its features, and one that represents the intended target
-    # using its position.
-    # This is the first option.
+    # using its features (target_rep="features"), 
+    # and one that represents the intended target using its position
+    # (target_rep="position").
 
-    def __init__(self, games=None, hidden_dim=5):
+    def __init__(self, games=None, hidden_dim=5, target_rep="features"):
         self.games = games
         self.n_targets = len(games[0].targets)
         self.n_features = len(games[0].messages)
-        self.input_dim = self.n_targets * self.n_features + self.n_features
+        self.target_rep = target_rep
+
+        if target_rep == "features":
+            self.input_dim = self.n_targets * self.n_features + self.n_features
+        elif target_rep == "position":
+            self.input_dim = self.n_targets * self.n_features + self.n_targets
+
         self.ann = ShallowNeuralNetwork(input_dim=self.input_dim,
                                         hidden_dim=hidden_dim,
                                         output_dim=self.n_features)
@@ -74,9 +81,15 @@ class ANNSpeaker(object):
     def message_vec(self, game, target):
         targets = game.targets
         input_targets = sum(targets.values(), [])
-        input_target_features = game.targets[target]
 
-        return self.ann.predict(input_targets + input_target_features)
+        if self.target_rep == "features":
+            input_target_nodes = game.targets[target]
+        elif self.target_rep == "position":
+            input_target_nodes = [0 for i in range(len(targets))]
+            indx = targets.keys().index(target)
+            input_target_nodes[indx] = 1
+
+        return self.ann.predict(input_targets + input_target_nodes)
 
     def produce(self, game, target):
         # produces a target (from Targets dict)
@@ -106,8 +119,14 @@ class ANNSpeaker(object):
             for target in targets.keys():
                 for message in messages.keys():
                     if target == listener.interpret(game, message):
-                        input_target_features = targets[target]
-                        input_ann = input_targets + input_target_features
+                        if self.target_rep == "features":
+                            input_target_nodes = targets[target]
+                        elif self.target_rep == "position":
+                            input_target_nodes = [0 for i in range(len(targets))]
+                            indx = targets.keys().index(target)
+                            input_target_nodes[indx] = 1 
+
+                        input_ann = input_targets + input_target_nodes
                         output_ann = game.messages[message]
                         data.append((input_ann, output_ann))
         if verbose:
@@ -149,11 +168,13 @@ class LitListener(object):
 
 def SelfTrain(games, max_iter = 2, verbose = True,
               speaker_hidden_dim=10, listener_hidden_dim=10,
+              speaker_target_rep="features",
               chain_init="literal_speaker"):
     if verbose:
         print "initialize ANN listeners and speakers"
     listener = ANNListener(games, hidden_dim=listener_hidden_dim)
-    speaker = ANNSpeaker(games, hidden_dim=speaker_hidden_dim)
+    speaker = ANNSpeaker(games, hidden_dim=speaker_hidden_dim, 
+                         target_rep=speaker_target_rep)
     if verbose:
         print "done"
 
@@ -248,3 +269,11 @@ def EvaluateListener(listener, refs):
     
     return correct / N
 
+def SummaryEvaluateListener(listener):
+    print "Listener accuracies on level0 instances: ",
+    print "{0} of {1}".format(EvaluateListener(listener, level0), len(level0))
+    print "Listener accuracies on level1 instances: ",
+    print "{0} of {1}".format(EvaluateListener(listener, level1), len(level1))
+    print "Listener accuracies on level2 instances: ",
+    print "{0} of {1}".format(EvaluateListener(listener, level2), len(level2))
+    
--- ibr_classifier.py
+++ ibr_classifier.py
@@ -1,100 +1,99 @@
 def ibr_classifier(game, message, target, depth=2):
-	if target not in game.sems[message]:
-		return -1
-	else:
-	
-		l0 = {}
-		for m in game.messages.keys():
-			l0[m] = {}
-			for t in game.targets.keys():
-				if t in game.sems[m]:
-					l0[m][t] = 1.0/len(game.sems[m])
-				else:
-					l0[m][t] = 0
-					
-		messages_per_target = {}
-		for t in game.targets.keys():
-			messages_per_target[t] = 0
-			for m in game.messages.keys():
-				if t in game.sems[m]:
-					messages_per_target[t] += 1
-					
-		s0 = {}
-		for t in game.targets.keys():
-			s0[t] = {}
-			for m in game.messages.keys():
-				if t in game.sems[m]:
-					s0[t][m] = 1.0/messages_per_target[t]
-				else:
-					s0[t][m] = 0
-					
-		if is_solved(l0,message,target):
-			return 0
-		
-		else:
-		
-			current_listener = l0
-			current_speaker = s0
-			current_depth = 1
-		
-			while current_depth <= depth:
-				new_listener, new_speaker = posterior(game,current_listener,current_speaker)
-				
-				if is_solved(new_listener,message,target):
-					return current_depth
-				
-				else:
-					current_listener = new_listener
-					current_speaker = new_speaker
-					current_depth += 1
-					
-			return -1
+    if target not in game.sems[message]:
+        return -1
+    else:
+        l0 = {}
+        for m in game.messages.keys():
+            l0[m] = {}
+            for t in game.targets.keys():
+                if t in game.sems[m]:
+                    l0[m][t] = 1.0/len(game.sems[m])
+                else:
+                    l0[m][t] = 0
+                    
+        messages_per_target = {}
+        for t in game.targets.keys():
+            messages_per_target[t] = 0
+            for m in game.messages.keys():
+                if t in game.sems[m]:
+                    messages_per_target[t] += 1
+                    
+        s0 = {}
+        for t in game.targets.keys():
+            s0[t] = {}
+            for m in game.messages.keys():
+                if t in game.sems[m]:
+                    s0[t][m] = 1.0/messages_per_target[t]
+                else:
+                    s0[t][m] = 0
+                    
+        if is_solved(l0,message,target):
+            return 0
+        
+        else:
+        
+            current_listener = l0
+            current_speaker = s0
+            current_depth = 1
+        
+            while current_depth <= depth:
+                new_listener, new_speaker = posterior(game,current_listener,current_speaker)
+                
+                if is_solved(new_listener,message,target):
+                    return current_depth
+                
+                else:
+                    current_listener = new_listener
+                    current_speaker = new_speaker
+                    current_depth += 1
+                    
+            return -1
 
 def is_solved(ibr_dict, message, target):
-	target_val = ibr_dict[message][target]
-	solved = True
-	for t in ibr_dict[message]: 
-		if t!= target and ibr_dict[message][t] >= target_val:
-			solved = False
-	return solved
-			
+    target_val = ibr_dict[message][target]
+    solved = True
+    for t in ibr_dict[message]: 
+        if t!= target and ibr_dict[message][t] >= target_val:
+            solved = False
+    return solved
+            
 # listener_dict = {'all':{'1':0.0,'2':1.0},'some':{'1':0.5,'2':0.5}}
-# speaker_dict = {'1':{'all':0.0,'some':1.0},'2':{'all':0.5,'some':0.5}}		
-# target_probs = {'1':0.5,'2':0.5}	
+# speaker_dict = {'1':{'all':0.0,'some':1.0},'2':{'all':0.5,'some':0.5}}        
+# target_probs = {'1':0.5,'2':0.5}    
 # message_probs = {'all':0.5,'some':0.5}
-			
+            
 def posterior(game, listener_dict, speaker_dict,target_probs=None,message_probs=None):
-	targets = game.targets.keys()
-	messages = game.messages.keys()
-	
-	new_listener_dict = {}
-	new_speaker_dict = {}
-	
-	if not target_probs:	# Assume uniform prior distribution unless otherwise specified
-		target_probs = {}
-		for t in targets:
-			target_probs[t] = 1.0/len(targets)
-	
-	if not message_probs:	# Assume uniform prior distribution unless otherwise specified
-		message_probs = {}
-		for m in messages:
-			message_probs[m] = 1.0/len(messages)
-	
-	for m in messages:
-		new_listener_dict[m] = {}
-		sum = 0.0
-		for t in targets:
-			sum += target_probs[t] * speaker_dict[t][m]
-		for t in targets:
-			new_listener_dict[m][t] = target_probs[t] * speaker_dict[t][m]/sum if sum else 0
-		
-	for t in targets:
-		new_speaker_dict[t] = {}
-		sum = 0.0
-		for m in messages:
-			sum += message_probs[m] * listener_dict[m][t]
-		for m in messages:
-			new_speaker_dict[t][m] = message_probs[m] * listener_dict[m][t]/sum if sum else 0
-			
-	return (new_listener_dict,new_speaker_dict)
+    targets = game.targets.keys()
+    messages = game.messages.keys()
+    
+    new_listener_dict = {}
+    new_speaker_dict = {}
+    
+    if not target_probs:    # Assume uniform prior distribution unless otherwise specified
+        target_probs = {}
+        for t in targets:
+            target_probs[t] = 1.0/len(targets)
+    
+    if not message_probs:    # Assume uniform prior distribution unless otherwise specified
+        message_probs = {}
+        for m in messages:
+            message_probs[m] = 1.0/len(messages)
+    
+    for m in messages:
+        new_listener_dict[m] = {}
+        sum = 0.0
+        for t in targets:
+            sum += target_probs[t] * speaker_dict[t][m]
+        for t in targets:
+            new_listener_dict[m][t] = target_probs[t] * speaker_dict[t][m]/sum if sum else 0
+        
+    for t in targets:
+        new_speaker_dict[t] = {}
+        sum = 0.0
+        for m in messages:
+            sum += message_probs[m] * listener_dict[m][t]
+        for m in messages:
+            new_speaker_dict[t][m] = message_probs[m] * listener_dict[m][t]/sum if sum else 0
+            
+    return (new_listener_dict,new_speaker_dict)
 
