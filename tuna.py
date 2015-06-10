4d1de507b140d1b272277e2955a0370b40f519d2
Naomi T. S <tsnaomi@uw.edu>
2015-06-09 17:59:29 -0700

    merge Tuna and RobustTuna

--- tuna.py
+++ tuna.py
@@ -4,14 +4,11 @@ import xml.etree.ElementTree as ET
 from games import Game
 from ibr_classifier import ibr_classifier
 from itertools import combinations, product, repeat
-from math import sqrt
 from pdb import set_trace
 from pprint import pprint
 
 
 CORPUS = 'TUNA/corpus/singular/furniture'
-
-# TODO: Implement object inheritance
 
 
 # 14-dimensional vectors
@@ -19,392 +16,22 @@ class Tuna:
 
     def __init__(self, corpus=CORPUS):
         self.games = []
+
+        # this contains every possible reference instance given the
+        # game contexts provided in TUNA and every possible message
+        # that is true of the target referent
         self.reference_instances = []
         self.unsolvable = []
         self.level0 = []
         self.level1 = []
         self.level2 = []
 
-        self.TunaFeatures = [
-            'chair', 'sofa', 'desk', 'fan',  # type
-            'blue', 'red', 'green', 'grey',  # colour
-            'left', 'right', 'front', 'back',  # orientation
-            'large', 'small',  # size
-            ]
-
-        self.T = 7  # each context contains 1 target and 6 distractors
-        self.F = 14
-
-        self.gather_reference_instances()
-        self.classify()
-
-    def key(self, x):
-        # indices of type, colour, orientation, and size
-        return self.TunaFeatures.index(x)
-
-    def gather_reference_instances(self):
-        for dirpath, dirname, filenames in os.walk(CORPUS):
-
-            for f in filenames:
-                filepath = dirpath + '/' + f
-                ref_inst = self.create_reference_instance(filepath)
-
-                if ref_inst:
-                    self.reference_instances.append(ref_inst)
-
-    def create_reference_instance(self, filepath):
-        try:
-            tree = ET.parse(filepath)
-            root = tree.getroot()
-
-            # create targets
-            target, referents = self._get_target_and_referents(root)
-
-            # create message
-            M, message = self._get_complex_message(root)
-
-            # create messages
-            messages = self._get_complex_messages_dict(M)
-
-            if message not in messages.keys():
-                raise KeyError('%s has ambiguous attributes.' % filepath)
-
-            # create semantics
-            semantics = self._get_semantics_dict(referents, messages)
-
-            # create game
-            game = Game(targets=referents, messages=messages, sems=semantics)
-
-            # create reference instance
-            ref_inst = {'game': game, 'message': message, 'target': target}
-
-            # add game to list of all games
-            self.games.append(game)
-
-            return ref_inst
-
-        # ignore non-xml files
-        except ET.ParseError:
-            pass
-
-        # ignore xml files that have multiple values for a single attribute
-        except KeyError as e:
-            print e
-
-    def _get_target_and_referents(self, root):
-        # create referents
-        referents = {}
-
-        for entity in root.iter('ENTITY'):
-            referent = entity.attrib['ID']
-            vector = list(repeat(0, self.F))
-
-            attributes = entity.getchildren()
-
-            # set target
-            if entity.attrib['TYPE'] == 'target':
-                target = referent
-
-            # colour, type, orientation, and size attributes
-            for attr in attributes[:4]:
-                vector[self.key(attr.attrib['VALUE'])] = 1
-
-            referents[referent] = vector
-
-        return target, referents
-
-    def _get_complex_message(self, root):
-
-        def non_dimension(a):
-            return a.attrib['NAME'] not in 'x-dimension y-dimension'
-
-        attr_set = root.find('ATTRIBUTE-SET').getchildren()
-        attr_set = [a.attrib['VALUE'] for a in attr_set if non_dimension(a)]
-        attr_set = filter(lambda a: a not in 'other unknown', attr_set)
-        attr_set = [a for a in sorted(attr_set, key=self.key)]
-
-        M = len(attr_set)
-
-        message = ' '.join(attr_set)
-
-        return M, message
-
-    def _get_complex_messages_dict(self, M):
-
-        def create_features_matrix(f):
-            messages = [list(repeat(0, f)) for i in range(f + 1)]
-
-            for i in range(f):
-                messages[i][i] = 1
-
-            return messages
-
-        def flatten(li):
-            return [i for feature in li for i in feature]
-
-        def from_vector_to_message(v):
-            message = [self.TunaFeatures[i] for i, w in enumerate(v) if w]
-
-            return ' '.join(message)
-
-        FeaturesMatrices = [
-            create_features_matrix(4),  # type
-            create_features_matrix(4),  # colour
-            create_features_matrix(4),  # orientation
-            create_features_matrix(2),  # size
-            ]
-
-        messages = [flatten(p) for p in product(*FeaturesMatrices)]
-        messages = filter(lambda m: m.count(1) == M, messages)
-        messages_dict = {from_vector_to_message(v): v for v in messages}
-
-        return messages_dict
-
-    def _get_semantics_dict(self, referents, messages):
-        semantics = {m: [] for m in messages}
-
-        for mk, mv in messages.iteritems():
-
-            for tk, tv in referents.iteritems():
-                if len(filter(None, map(lambda t, m: t * m, tv, mv))) == 3:
-                    semantics[mk].append(tk)
-
-        return semantics
-
-    def classify(self):
-        level = {-1: [], 0: [], 1: [], 2: []}
-
-        for ref_inst in self.reference_instances:
-            level[ibr_classifier(ref_inst)].append(ref_inst)
-
-        self.unsolvable.extend(level[-1])
-        self.level0.extend(level[0])
-        self.level1.extend(level[1])
-        self.level2.extend(level[2])
-
-
-# 16-dimensional vectors
-class DimensionalTuna:
-
-    def __init__(self, corpus=CORPUS):
-        self.games = []
-        self.reference_instances = []
-        self.unsolvable = []
-        self.level0 = []
-        self.level1 = []
-        self.level2 = []
-
-        self.TunaFeatures = [
-            'chair', 'sofa', 'desk', 'fan',  # type
-            'blue', 'red', 'green', 'grey',  # colour
-            'left', 'right', 'front', 'back',  # orientation
-            'large', 'small',  # size
-            'x-dimension',  # x-dimension
-            'y-dimension',  # y-dimension
-            ]
-
-        self.T = 7  # each context contains 1 target and 6 distractors
-        self.F = 16
-
-        self.gather_reference_instances()
-        self.classify()
-
-    def key(self, x):
-        try:
-            # indices of type, colour, orientation, and size
-            return self.TunaFeatures.index(x)
-
-        except ValueError:
-            # indices of x- and y- dimensions
-            return 14 if int(x) in [0, 1, 2, 3, 4, 5] else 15
-
-    def gather_reference_instances(self):
-        for dirpath, dirname, filenames in os.walk(CORPUS):
-
-            for f in filenames:
-                filepath = dirpath + '/' + f
-                ref_inst = self.create_reference_instance(filepath)
-
-                if ref_inst:
-                    self.reference_instances.append(ref_inst)
-
-    def create_reference_instance(self, filepath):
-        try:
-            tree = ET.parse(filepath)
-            root = tree.getroot()
-
-            # create targets
-            target, referents = self._get_target_and_referents(root)
-
-            # create message
-            M, message = self._get_complex_message(root)
-
-            # create messages
-            messages = self._get_complex_messages_dict(M)
-
-            if message not in messages.keys():
-                raise KeyError('%s has ambiguous attributes.' % filepath)
-
-            # create semantics
-            semantics = self._get_semantics_dict(referents, messages)
-
-            # create game
-            game = Game(targets=referents, messages=messages, sems=semantics)
-
-            # create reference instance
-            ref_inst = {'game': game, 'message': message, 'target': target}
-
-            # add game to list of all games
-            self.games.append(game)
-
-            return ref_inst
-
-        # ignore non-xml files
-        except ET.ParseError:
-            pass
-
-        # ignore xml files that have multiple values for a single attribute
-        except KeyError as e:
-            print e
-
-    def _get_target_and_referents(self, root):
-        # create referents
-        referents = {}
-
-        for entity in root.iter('ENTITY'):
-            referent = entity.attrib['ID']
-            vector = list(repeat(0, self.F))
-
-            attributes = entity.getchildren()
-
-            # set target
-            if entity.attrib['TYPE'] == 'target':
-                target = referent
-
-            # colour, type, orientation, and size attributes
-            for attr in attributes[:4]:
-                vector[self.key(attr.attrib['VALUE'])] = 1
-
-            # x-dimension attribute
-            try:
-                vector[14] = int(attributes[4].attrib['VALUE'])
-            except ValueError:
-                vector[14] = 0
-
-            # y-dimension attribute
-            try:
-                vector[15] = int(attributes[5].attrib['VALUE']) + 5
-                # y = y + 5 if y != 0 else -1
-                # vector[15] = y
-            except ValueError:
-                vector[15] = -1
-
-            referents[referent] = vector
-
-        return target, referents
-
-    def _get_complex_message(self, root):
-        attr_set = []
-        attributes = root.find('ATTRIBUTE-SET').getchildren()
-
-        for a in attributes:
-
-            try:
-                num = int(a.attrib['VALUE'])
-
-                if a.attrib['NAME'] == 'y-dimension':
-                    num = -1 if num == 0 else num + 5
-
-                attr_set.append(str(num))
-
-            except ValueError:
-                attr_set.append(a.attrib['VALUE'])
-
-        attr_set = filter(lambda a: a not in 'other unknown', attr_set)
-        attr_set = [a for a in sorted(attr_set, key=self.key)]
-
-        M = len(attr_set)
-
-        message = ' '.join(attr_set)
-
-        return M, message
-
-    def _get_complex_messages_dict(self, M):
-
-        def create_features_matrix(f):
-            messages = [list(repeat(0, f)) for i in range(f + 1)]
-
-            for i in range(f):
-                messages[i][i] = 1
-
-            return messages
-
-        def flatten(li):
-            return [i for feature in li for i in feature]
-
-        def is_viable(m):
-            return m.count(0) + m.count(-1) == self.F - M,
-
-        def from_vector_to_message(v):
-            message = [self.TunaFeatures[i] for i, w in enumerate(v[:-2]) if w]
-            message += [str(num) for num in v[-2:] if num > 0]
-
-            return ' '.join(message)
-
-        FeaturesMatrices = [
-            create_features_matrix(4),  # type
-            create_features_matrix(4),  # colour
-            create_features_matrix(4),  # orientation
-            create_features_matrix(2),  # size
-            [[0, ], [1, ], [2, ], [3, ], [4, ], [5, ]],  # x_dimension
-            [[-1, ], [6, ], [7, ], [8, ]],  # y-dimension
-            ]
-
-        messages = [flatten(p) for p in product(*FeaturesMatrices)]
-        messages = filter(is_viable, messages)
-        messages_dict = {from_vector_to_message(v): v for v in messages}
-
-        return messages_dict
-
-    def _get_semantics_dict(self, referents, messages):
-
-        def is_square(n):
-            return sqrt(abs(n)) == int(sqrt(abs(n)))
-
-        semantics = {m: [] for m in messages}
-
-        for mk, mv in messages.iteritems():
-
-            for tk, tv in referents.iteritems():
-                product = filter(None, map(lambda t, m: t * m, tv, mv))
-
-                if len(product) == 3 and all([is_square(p) for p in product]):
-                    semantics[mk].append(tk)
-
-        return semantics
-
-    def classify(self):
-        level = {-1: [], 0: [], 1: [], 2: []}
-
-        for ref_inst in self.reference_instances:
-            level[ibr_classifier(ref_inst)].append(ref_inst)
-
-        self.unsolvable.extend(level[-1])
-        self.level0.extend(level[0])
-        self.level1.extend(level[1])
-        self.level2.extend(level[2])
-
-
-# 14-dimensional vectors
-class RobustTuna:
-
-    def __init__(self, corpus=CORPUS):
-        self.games = []
-        self.reference_instances = []
-        self.unsolvable = []
-        self.level0 = []
-        self.level1 = []
-        self.level2 = []
+        # this contains only the reference instances provided in TUNA
+        self.true_reference_instances = []
+        self.true_unsolvable = []
+        self.true_level0 = []
+        self.true_level1 = []
+        self.true_level2 = []
 
         self.TunaFeatures = [
             'chair', 'sofa', 'desk', 'fan',  # type
@@ -426,6 +53,9 @@ class RobustTuna:
         return self.TunaFeatures.index(x)
 
     def curate_complex_messages_dict(self):
+        # this functin curates self.ComplexMessages, a dictionary that contains
+        # every possible message of length 1 through 4 given the features in
+        # self.TunaFeatures
 
         def create_features_matrix(f):
             messages = [list(repeat(0, f)) for i in range(f + 1)]
@@ -458,17 +88,19 @@ class RobustTuna:
             self.ComplexMessages.update(messages_dict)
 
     def gather_reference_instances(self):
+        # this function combs through TUNA and collects all of its games and
+        # possible reference instance, extending self.reference_instances and
+        # self.true_reference_instances through create_reference_instances()
         for dirpath, dirname, filenames in os.walk(CORPUS):
 
             for f in filenames:
                 filepath = dirpath + '/' + f
-                ref_instances = list(self.create_reference_instance(filepath))
-                ref_instances = filter(None, ref_instances)
+                self.create_reference_instances(filepath)
 
-                if ref_instances:
-                    self.reference_instances.extend(ref_instances)
-
-    def create_reference_instance(self, filepath):
+    def create_reference_instances(self, filepath):
+        # given an xml file in TUNA, this function produces the game and every
+        # possible reference instance in that file, extending
+        # self.reference_instances, self.true_references_instances, etc.
         try:
             tree = ET.parse(filepath)
             root = tree.getroot()
@@ -496,6 +128,9 @@ class RobustTuna:
                     sems=semantics,
                     )
 
+                # get true complex message from xml file
+                true_message = self._get_true_complex_message(root)
+
                 # add game to list of all games
                 self.games.append(game)
 
@@ -508,19 +143,52 @@ class RobustTuna:
                         'target': target,
                         }
 
-                    # classify ref_inst with ibr_classifier
+                    # classify ref_inst with ibr_classifier and add to either
+                    # self.unsolvable, self.level0, self.level1, etc.,
+                    # depending on its classification
                     try:
-                        level = ibr_classifier(ref_inst)
-                        getattr(self, 'level%s' % level).append(ref_inst)
+                        level = ibr_classifier(ref_inst, arg_max=True)
+                        level = 'level%s' % level
+                        getattr(self, level).append(ref_inst)
 
                     except AttributeError:
                         self.unsolvable.append(ref_inst)
 
-                    yield ref_inst
+                    # add ref_inst to self.reference_instances
+                    self.reference_instances.append(ref_inst)
+
+                    # curate true reference instances
+                    if message == true_message:
+
+                        # add ref_inst to self.reference_instances
+                        self.true_reference_instances.append(ref_inst)
+
+                        # add ref_inst to either self.true_unsolvable,
+                        # self.true_level0, self.true_level1, etc., depending
+                        # on its ibr classification
+                        try:
+                            getattr(self, 'true_%s' % level).append(ref_inst)
+
+                        except AttributeError:
+                            self.true_unsolvable.append(ref_inst)
 
         # ignore non-xml files
         except ET.ParseError:
             pass
+
+    def _get_true_complex_message(self, root):
+
+        def non_dimension(a):
+            return a.attrib['NAME'] not in 'x-dimension y-dimension'
+
+        # given a TUNA file, grab and normalize the message produced by an
+        # actual speaker (located at the bottom of each xml file)
+        attr_set = root.find('ATTRIBUTE-SET').getchildren()
+        attr_set = [a.attrib['VALUE'] for a in attr_set if non_dimension(a)]
+        attr_set = filter(lambda a: a not in 'other unknown', attr_set)
+        message = ' '.join([a for a in sorted(attr_set, key=self.key)])
+
+        return message
 
     def _get_target_and_referents(self, root):
         # create referents
@@ -575,5 +243,5 @@ class RobustTuna:
 
 
 if __name__ == '__main__':
-    robust = RobustTuna()
+    tuna = Tuna()
     set_trace()
